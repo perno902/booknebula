bbbb0bcdc4059e18c16e83d777add65f048de6f0
Per Nordfors <perno902@student.liu.se>
2015-04-12 13:46:48 +0200

    Added page and controller for writing reviews

--- static/js/script.js
+++ static/js/script.js
@@ -65,6 +65,11 @@ myApp.config(['$routeProvider',
             templateUrl: '../static/pages/review.html',
             controller: 'reviewCtrl'
         })
+    // Review page
+        .when('/writeReview', {
+            templateUrl: '../static/pages/writeReview.html',
+            controller: "writeReviewCtrl"
+        })
 }]);
 
 // Create the controllers
@@ -306,3 +311,15 @@ myApp.factory('author', function() {
         books: books
     };
 });
+
+
+myApp.controller('writeReviewCtrl', ['$scope', 'title', function($scope, title) {
+    $scope.bookTitle = title.title;
+    $scope.year = title.year;
+    $scope.revTitle = '';
+    $scope.score = 0;
+    $scope.points = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    $scope.content = '';
+    $scope.language = '';
+    $scope.languages = ['English', 'French', 'German', 'Russian', 'Spanish', 'Swedish'];
+}]);
