3d614bb711427b63324b16f444ebe6689c4b46a0
Per Nordfors <perno902@student.liu.se>
2015-04-15 11:16:21 +0200

    Added Jasmine folder for unit testing

---
+++ jasmine/spec/toplistServiceSpec.js
@@ -0,0 +1,15 @@
+'use strict'
+
+describe('toplistService', function() {
+
+
+    beforeEach(module('myApp'));
+
+    describe('books', function(){
+
+        it('should be books', function() {
+            expect(true).toBe(true);
+        });
+
+    });
+});
