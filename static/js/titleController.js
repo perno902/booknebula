abd1c589a7db26342d086bef2f697aa4dd3dc603
Per Nordfors <perno902@student.liu.se>
2015-04-27 16:23:30 +0200

    Moved data variables for Title to service from ctrl

--- static/js/titleController.js
+++ static/js/titleController.js
@@ -1,43 +1,20 @@
 myApp.controller('titleCtrl', ['$scope', '$routeParams', 'title', function($scope, $routeParams, title) {
     $scope.bookId = $routeParams.titleId;
-    setBookId();
-
-    function setBookId() {
-        title.setBookId($scope.bookId);
-    };
 
     loadRemoteData();
 
-    function applyRemoteData(bookData) {
-        $scope.title = bookData.title;
-        $scope.year = bookData.year;
-        $scope.authors = bookData.authors;
-        $scope.language = bookData.language;
-        //$scope.avgScore = bookData.avgScore;
-        $scope.plot = bookData.plot;
-        $scope.reviews = bookData.reviews;
-
-        if ($scope.authors.length > 1) {
-            $scope.multipleAuthors = false;
-        }
-    }
-
     function loadRemoteData() {
+        title.setBookId($scope.bookId);
         title.getBookData()
-            .then(
-                function(bookData) {
-                    console.log(bookData);
-                    applyRemoteData(bookData);
-                }
-        )
-    }
+    };
 
-    /*
-    $scope.title = title.title;
-    $scope.year = title.year;
-    $scope.author = title.author;
-    $scope.origLang = title.origLang;
-    $scope.avgScore = title.avgScore;
-    $scope.plot = title.plot;
-    $scope.reviews = title.reviews; */
+    $scope.title = function() {return title.title()};
+    $scope.year = function() {return title.year()};
+    $scope.authors = function() {return title.authors()};
+    $scope.language = function() {return title.language()};
+    //$scope.avgScore =
+    $scope.plot = function() {return title.plot()};
+    $scope.reviews = function() {return title.reviews()};
+    $scope.multipleAuthors = function() {return $scope.authors().length > 1};
+
 }]);
--- static/js/titleService.js
+++ static/js/titleService.js
@@ -1,6 +1,12 @@
 myApp.factory('title', [ '$http', function($http) {
     var bookId = '';
-    console.log('inside service');
+    var title = '';
+    var year = '';
+    var authors= [];
+    var language = '';
+    var avgScore = '';
+    var plot = '';
+    var reviews = [];
 
     function setBookId(id) {
         bookId = id;
@@ -16,42 +22,38 @@ myApp.factory('title', [ '$http', function($http) {
             }
         });
         return (request.then(handleSuccess, handleError));
-    }
+    };
 
     function handleError(response) {
         console.log('error');
-    }
+    };
 
     function handleSuccess(response) {
         console.log('success');
-        return response.data.data;
-    }
-
-    return {
-        setBookId: setBookId,
-        getBookData: getBookData
+        applyRemoteData(response.data.data);
     };
 
-    /*var title = 'Lolita';
-    var year = 1955;
-    var author = 'Vladimir Nabokov';
-    var origLang = 'English';
-    var avgScore = 8.7;
-    var plot = 'A man marries his landlady so he can take advantage of her daughter.' +
-                    ' Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras arcu eros, ' +
-                    'eleifend vitae urna et tristique dapibus eros. Praesent at posuere ipsum, eu blandit dui. '
-    var reviews = [
-        {id: 23, upvotes: 46, reviewer: 'n0rp3r_the_critic', revTitle: 'A great book!', score: 10, date: '2015-02-11'},
-        {id: 24, upvotes: 2, reviewer: 'frau_blucher', revTitle: 'This book is filth!', score: 1, date: '2012-06-29'}
-        ];
+    function applyRemoteData(data) {
+        title = data.title;
+        year = data.year;
+        authors = data.authors;
+        language = data.language;
+        //avgScore = data.avgScore;
+        plot = data.plot;
+        reviews = data.reviews;
+    };
+
     return {
         setBookId: setBookId,
-        title: title,
-        year: year,
-        author: author,
-        origLang: origLang,
-        avgScore: avgScore,
-        plot: plot,
-        reviews: reviews
-    };*/
+        getBookData: getBookData,
+        title: function() {return title},
+        year: function() {return year},
+        authors: function() {return authors},
+        language: function() {return language},
+        avgScore: function() {return avgScore},
+        plot: function() {return plot},
+        reviews: function() {return reviews}
+
+    };
+
 }]);
